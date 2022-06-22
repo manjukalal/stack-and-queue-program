6f2598eda515f29e81e99c878f7f0d79def1a52b
manjukalal <manjukalal1496@gmail.com>
2022-06-22 14:08:53 +0530

    Added:pushed node data to stack

---
+++ src/com/bridgelabz/StackLogic.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+import java.util.LinkedList;
+
+public class StackLogic {
+    LinkedList<Integer> stack = new LinkedList<>();
+
+    public void push(int data) {
+        stack.addFirst(data);
+    }
+
+    public void myPrint() {
+        System.out.println("Stacklist is :");
+        for (Integer i : stack) {
+            System.out.println(" " + i);
+        }
+    }
+}
+
+
+
+
